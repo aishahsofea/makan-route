16a3c83471ced1cb658a949f873503a739375183
unknown <2020kumarankit@gmail.com>
2025-05-03 21:19:28 +0800

    dummy data displayed on api call

--- src/app/api/makan-spots/route.ts
+++ src/app/api/makan-spots/route.ts
@@ -1,2 +1,14 @@
-import { NextResponse } from 'next/server';
-export async function GET() { return NextResponse.json({ works: true }); }
+import { NextResponse } from "next/server";
+import { makanSpots } from "@/data/dummy-spots";
+
+export async function GET() {
+    return new NextResponse(JSON.stringify(makanSpots), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Cache-Control': 'no-cache, no-store, must-revalidate'
+      }
+    });
+  }
+  
